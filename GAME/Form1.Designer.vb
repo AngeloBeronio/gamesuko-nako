78727c840fa6eea1e2efd93c12b378fbc17dac71
Yuan <caro.dennis1@gmail.com>
2026-02-05 11:16:18 +0800

    Add project files.

---
+++ GAME/Form1.Designer.vb
@@ -0,0 +1,325 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        Button1 = New Button()
+        PictureBox5 = New PictureBox()
+        Timer1 = New Timer(components)
+        Cat1 = New PictureBox()
+        Mango = New PictureBox()
+        Red1 = New PictureBox()
+        Red2 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        PictureBox10 = New PictureBox()
+        Red3 = New PictureBox()
+        Red4 = New PictureBox()
+        Red5 = New PictureBox()
+        Red6 = New PictureBox()
+        Button2 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Cat1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Mango, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Red6, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.ActiveCaptionText
+        PictureBox1.Location = New Point(182, 112)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(1171, 19)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.ActiveCaptionText
+        PictureBox2.Location = New Point(182, 660)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(1171, 19)
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = SystemColors.ActiveCaptionText
+        PictureBox3.Location = New Point(182, 127)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(22, 536)
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackColor = SystemColors.ActiveCaptionText
+        PictureBox4.Location = New Point(1331, 127)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(22, 536)
+        PictureBox4.TabIndex = 3
+        PictureBox4.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(385, 685)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(197, 83)
+        Button1.TabIndex = 4
+        Button1.Text = "HARD MODE"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.BackColor = SystemColors.ActiveCaption
+        PictureBox5.Location = New Point(239, 374)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(34, 35)
+        PictureBox5.TabIndex = 5
+        PictureBox5.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Cat1
+        ' 
+        Cat1.Image = My.Resources.Resources.CAT
+        Cat1.Location = New Point(833, 137)
+        Cat1.Name = "Cat1"
+        Cat1.Size = New Size(288, 176)
+        Cat1.TabIndex = 6
+        Cat1.TabStop = False
+        ' 
+        ' Mango
+        ' 
+        Mango.Image = My.Resources.Resources.MANGO_ICECREAM
+        Mango.Location = New Point(1173, 137)
+        Mango.Name = "Mango"
+        Mango.Size = New Size(143, 126)
+        Mango.TabIndex = 8
+        Mango.TabStop = False
+        ' 
+        ' Red1
+        ' 
+        Red1.BackColor = Color.Brown
+        Red1.Location = New Point(589, 137)
+        Red1.Name = "Red1"
+        Red1.Size = New Size(71, 64)
+        Red1.TabIndex = 9
+        Red1.TabStop = False
+        ' 
+        ' Red2
+        ' 
+        Red2.BackColor = Color.Brown
+        Red2.Location = New Point(458, 590)
+        Red2.Name = "Red2"
+        Red2.Size = New Size(71, 64)
+        Red2.TabIndex = 10
+        Red2.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackColor = SystemColors.ActiveCaptionText
+        PictureBox6.Location = New Point(743, 127)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(22, 106)
+        PictureBox6.TabIndex = 11
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.BackColor = SystemColors.ActiveCaptionText
+        PictureBox7.Location = New Point(743, 557)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(22, 106)
+        PictureBox7.TabIndex = 12
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = SystemColors.ActiveCaptionText
+        PictureBox8.Location = New Point(330, 127)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(22, 106)
+        PictureBox8.TabIndex = 13
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.BackColor = SystemColors.ActiveCaptionText
+        PictureBox9.Location = New Point(330, 557)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(22, 106)
+        PictureBox9.TabIndex = 14
+        PictureBox9.TabStop = False
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.BackColor = SystemColors.ActiveCaptionText
+        PictureBox10.Location = New Point(1136, 127)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(22, 417)
+        PictureBox10.TabIndex = 15
+        PictureBox10.TabStop = False
+        ' 
+        ' Red3
+        ' 
+        Red3.BackColor = Color.Brown
+        Red3.Location = New Point(1164, 283)
+        Red3.Name = "Red3"
+        Red3.Size = New Size(37, 18)
+        Red3.TabIndex = 16
+        Red3.TabStop = False
+        ' 
+        ' Red4
+        ' 
+        Red4.BackColor = Color.Brown
+        Red4.Location = New Point(1288, 365)
+        Red4.Name = "Red4"
+        Red4.Size = New Size(37, 18)
+        Red4.TabIndex = 17
+        Red4.TabStop = False
+        ' 
+        ' Red5
+        ' 
+        Red5.BackColor = Color.Brown
+        Red5.Location = New Point(1164, 443)
+        Red5.Name = "Red5"
+        Red5.Size = New Size(37, 18)
+        Red5.TabIndex = 18
+        Red5.TabStop = False
+        ' 
+        ' Red6
+        ' 
+        Red6.BackColor = Color.Brown
+        Red6.Location = New Point(1288, 526)
+        Red6.Name = "Red6"
+        Red6.Size = New Size(37, 18)
+        Red6.TabIndex = 19
+        Red6.TabStop = False
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(182, 685)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(197, 83)
+        Button2.TabIndex = 20
+        Button2.Text = "NORMAL MODE"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1520, 777)
+        Controls.Add(Button2)
+        Controls.Add(Red6)
+        Controls.Add(Red5)
+        Controls.Add(Red4)
+        Controls.Add(Red3)
+        Controls.Add(PictureBox10)
+        Controls.Add(PictureBox9)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox6)
+        Controls.Add(Red2)
+        Controls.Add(Red1)
+        Controls.Add(Mango)
+        Controls.Add(Cat1)
+        Controls.Add(PictureBox5)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(Cat1, ComponentModel.ISupportInitialize).EndInit()
+        CType(Mango, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red1, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red3, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red4, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red5, ComponentModel.ISupportInitialize).EndInit()
+        CType(Red6, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Cat1 As PictureBox
+    Friend WithEvents Mango As PictureBox
+    Friend WithEvents Red1 As PictureBox
+    Friend WithEvents Red2 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents Red5 As PictureBox
+    Friend WithEvents Red3 As PictureBox
+    Friend WithEvents Red4 As PictureBox
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents Red6 As PictureBox
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ GAME/Form1.vb
@@ -0,0 +1,185 @@
+﻿Public Class Form1
+
+    Dim goUp As Boolean = False
+    Dim goDown As Boolean = False
+    Dim goLeft As Boolean = False
+    Dim goRight As Boolean = False
+    Dim speed As Integer = 5
+
+    Dim canMove As Boolean = False
+    Dim enemySpeed As Integer = 6
+
+    Dim walls As New List(Of PictureBox)
+    Dim deathZones As New List(Of PictureBox)
+
+    Dim Red1GoingDown As Boolean = True
+    Dim Red2GoingDown As Boolean = False
+    Dim Red3GoingRight As Boolean = True
+    Dim Red4GoingRight As Boolean = False
+    Dim Red5GoingRight As Boolean = True
+    Dim Red6GoingRight As Boolean = False
+    Dim Cat1GoingDown As Boolean = True
+
+    Dim startX As Integer = 239
+    Dim startY As Integer = 374
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+        Timer1.Interval = 20
+        Timer1.Enabled = True
+
+        walls.Add(PictureBox1)
+        walls.Add(PictureBox2)
+        walls.Add(PictureBox3)
+        walls.Add(PictureBox4)
+        walls.Add(PictureBox6)
+        walls.Add(PictureBox7)
+        walls.Add(PictureBox8)
+        walls.Add(PictureBox9)
+        walls.Add(PictureBox10)
+
+        deathZones.Add(Red1)
+        deathZones.Add(Red2)
+        deathZones.Add(Red3)
+        deathZones.Add(Red4)
+        deathZones.Add(Red5)
+        deathZones.Add(Red6)
+        deathZones.Add(Cat1)
+
+        PictureBox5.Left = startX
+        PictureBox5.Top = startY
+    End Sub
+
+    Private Sub StartGame(Optional easyMode As Boolean = False)
+        canMove = True
+        enemySpeed = If(easyMode, 4, 6)
+
+        PictureBox5.Left = startX
+        PictureBox5.Top = startY
+
+        Red1.Top = 137
+        Red2.Top = 590
+        Red3.Left = 1164
+        Red4.Left = 1288
+        Red5.Left = 1164
+        Red6.Left = 1288
+        Cat1.Top = 137
+
+        Red1GoingDown = True
+        Red2GoingDown = False
+        Red3GoingRight = True
+        Red4GoingRight = False
+        Red5GoingRight = True
+        Red6GoingRight = False
+        Cat1GoingDown = True
+
+        goUp = False
+        goDown = False
+        goLeft = False
+        goRight = False
+
+        Timer1.Enabled = True
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        StartGame(False)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        StartGame(True)
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If canMove Then
+            If e.KeyCode = Keys.W Then goUp = True
+            If e.KeyCode = Keys.S Then goDown = True
+            If e.KeyCode = Keys.A Then goLeft = True
+            If e.KeyCode = Keys.D Then goRight = True
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If canMove Then
+            If e.KeyCode = Keys.W Then goUp = False
+            If e.KeyCode = Keys.S Then goDown = False
+            If e.KeyCode = Keys.A Then goLeft = False
+            If e.KeyCode = Keys.D Then goRight = False
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If canMove Then
+            MoveVertical(Red1, 137, 590, Red1GoingDown, Red1GoingDown)
+            MoveVertical(Red2, 137, 590, Red2GoingDown, Red2GoingDown)
+            MoveVertical(Cat1, 137, 478, Cat1GoingDown, Cat1GoingDown)
+            MoveHorizontal(Red3, 1164, 1288, Red3GoingRight, Red3GoingRight)
+            MoveHorizontal(Red4, 1164, 1288, Red4GoingRight, Red4GoingRight)
+            MoveHorizontal(Red5, 1164, 1288, Red5GoingRight, Red5GoingRight)
+            MoveHorizontal(Red6, 1164, 1288, Red6GoingRight, Red6GoingRight)
+
+            If goUp Then MovePlayer(0, -speed)
+            If goDown Then MovePlayer(0, speed)
+            If goLeft Then MovePlayer(-speed, 0)
+            If goRight Then MovePlayer(speed, 0)
+        End If
+
+        If PictureBox5.Bounds.IntersectsWith(Mango.Bounds) Then
+            Timer1.Enabled = False
+            MessageBox.Show("You win!")
+            canMove = False
+        End If
+
+        For Each death As PictureBox In deathZones
+            If PictureBox5.Bounds.IntersectsWith(death.Bounds) Then
+                Timer1.Enabled = False
+                MessageBox.Show("You died!")
+                canMove = False
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub MoveVertical(enemy As PictureBox, minY As Integer, maxY As Integer, ByRef goingDown As Boolean, ByRef toggleFlag As Boolean)
+        If goingDown Then
+            enemy.Top += enemySpeed
+            If enemy.Top >= maxY Then goingDown = False
+        Else
+            enemy.Top -= enemySpeed
+            If enemy.Top <= minY Then goingDown = True
+        End If
+    End Sub
+
+    Private Sub MoveHorizontal(enemy As PictureBox, minX As Integer, maxX As Integer, ByRef goingRight As Boolean, ByRef toggleFlag As Boolean)
+        If goingRight Then
+            enemy.Left += enemySpeed
+            If enemy.Left >= maxX Then goingRight = False
+        Else
+            enemy.Left -= enemySpeed
+            If enemy.Left <= minX Then goingRight = True
+        End If
+    End Sub
+
+    Private Sub MovePlayer(dx As Integer, dy As Integer)
+        PictureBox5.Left += dx
+        PictureBox5.Top += dy
+
+        If CollidesWithWalls() Then
+            PictureBox5.Left -= dx
+            PictureBox5.Top -= dy
+        End If
+
+        If PictureBox5.Left < 0 Then PictureBox5.Left = 0
+        If PictureBox5.Top < 0 Then PictureBox5.Top = 0
+        If PictureBox5.Right > Me.ClientSize.Width Then PictureBox5.Left = Me.ClientSize.Width - PictureBox5.Width
+        If PictureBox5.Bottom > Me.ClientSize.Height Then PictureBox5.Top = Me.ClientSize.Height - PictureBox5.Height
+    End Sub
+
+    Private Function CollidesWithWalls() As Boolean
+        For Each wall As PictureBox In walls
+            If PictureBox5.Bounds.IntersectsWith(wall.Bounds) Then Return True
+        Next
+        Return False
+    End Function
+
+End Class
